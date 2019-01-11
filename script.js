225c4af282b9a3800535eee74a918f5e224be735
Laura Mølgaard Tams <lmolgaardtams@zendesk.com>
2019-01-11 16:44:10 +0100

    Refactor jQuery to vanilla JS

--- script.js
+++ script.js
@@ -1,129 +1,159 @@
-/*
- * jQuery v1.9.1 included
- */
 
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', function() {
 
   // social share popups
-  $(".share a").click(function(e) {
-    e.preventDefault();
-    window.open(this.href, "", "height = 500, width = 500");
+  [].forEach.call(document.querySelectorAll('.share a'), function(anchor) {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      window.open(this.href, '', 'height = 500, width = 500');
+    });
   });
 
   // show form controls when the textarea receives focus or backbutton is used and value exists
-  var $commentContainerTextarea = $(".comment-container textarea"),
-    $commentContainerFormControls = $(".comment-form-controls, .comment-ccs");
+  var commentContainerTextarea = document.querySelector('.comment-container textarea'),
+    commentContainerFormControls = document.querySelector('.comment-form-controls, .comment-ccs');
 
-  $commentContainerTextarea.one("focus", function() {
-    $commentContainerFormControls.show();
-  });
+  if (commentContainerTextarea) {
+    commentContainerTextarea.addEventListener('focus', function focusCommentContainerTextarea() {
+      commentContainerFormControls.style.display = 'block';
+      commentContainerTextarea.removeEventListener('focus', focusCommentContainerTextarea);
+    });
 
-  if ($commentContainerTextarea.val() !== "") {
-    $commentContainerFormControls.show();
+    if (commentContainerTextarea.value !== '') {
+      commentContainerFormControls.style.display = 'block';
+    }
   }
 
   // Expand Request comment form when Add to conversation is clicked
-  var $showRequestCommentContainerTrigger = $(".request-container .comment-container .comment-show-container"),
-    $requestCommentFields = $(".request-container .comment-container .comment-fields"),
-    $requestCommentSubmit = $(".request-container .comment-container .request-submit-comment");
+  var showRequestCommentContainerTrigger = document.querySelector('.request-container .comment-container .comment-show-container'),
+    requestCommentFields = document.querySelectorAll('.request-container .comment-container .comment-fields'),
+    requestCommentSubmit = document.querySelector('.request-container .comment-container .request-submit-comment');
 
-  $showRequestCommentContainerTrigger.on("click", function() {
-    $showRequestCommentContainerTrigger.hide();
-    $requestCommentFields.show();
-    $requestCommentSubmit.show();
-    $commentContainerTextarea.focus();
-  });
+  if (showRequestCommentContainerTrigger) {
+    showRequestCommentContainerTrigger.addEventListener('click', function() {
+      showRequestCommentContainerTrigger.style.display = 'none';
+      [].forEach.call(requestCommentFields, function(e) { e.style.display = 'block'; });
+      requestCommentSubmit.style.display = 'inline-block';
+
+      if (commentContainerTextarea) {
+        commentContainerTextarea.focus();
+      }
+    });
+  }
 
   // Mark as solved button
-  var $requestMarkAsSolvedButton = $(".request-container .mark-as-solved:not([data-disabled])"),
-    $requestMarkAsSolvedCheckbox = $(".request-container .comment-container input[type=checkbox]"),
-    $requestCommentSubmitButton = $(".request-container .comment-container input[type=submit]");
+  var requestMarkAsSolvedButton = document.querySelector('.request-container .mark-as-solved:not([data-disabled])'),
+    requestMarkAsSolvedCheckbox = document.querySelector('.request-container .comment-container input[type=checkbox]'),
+    requestCommentSubmitButton = document.querySelector('.request-container .comment-container input[type=submit]');
 
-  $requestMarkAsSolvedButton.on("click", function () {
-    $requestMarkAsSolvedCheckbox.attr("checked", true);
-    $requestCommentSubmitButton.prop("disabled", true);
-    $(this).attr("data-disabled", true).closest("form").submit();
+  requestMarkAsSolvedButton && requestMarkAsSolvedButton.addEventListener('click', function () {
+    requestMarkAsSolvedCheckbox.setAttribute('checked', true);
+    requestCommentSubmitButton.disabled = true;
+    this.setAttribute('data-disabled', true);
+    // Element.closest is supported in modern browsers and has a polyfill available for still in use outdated versions
+    // See https://caniuse.com/#feat=element-closest
+    // TODO: Should polyfill be installed? 
+    this.closest('form').submit();
   });
 
   // Change Mark as solved text according to whether comment is filled
-  var $requestCommentTextarea = $(".request-container .comment-container textarea");
+  var requestCommentTextarea = document.querySelector('.request-container .comment-container textarea');
 
-  $requestCommentTextarea.on("input", function() {
-    if ($requestCommentTextarea.val() !== "") {
-      $requestMarkAsSolvedButton.text($requestMarkAsSolvedButton.data("solve-and-submit-translation"));
-      $requestCommentSubmitButton.prop("disabled", false);
+  requestCommentTextarea && requestCommentTextarea.addEventListener('input', function() {
+    if (requestCommentTextarea.value !== '') {
+      if (requestMarkAsSolvedButton) {
+        requestMarkAsSolvedButton.innerText = requestMarkAsSolvedButton.getAttribute('data-solve-and-submit-translation');
+      }
+      requestCommentSubmitButton.disabled = false;
     } else {
-      $requestMarkAsSolvedButton.text($requestMarkAsSolvedButton.data("solve-translation"));
-      $requestCommentSubmitButton.prop("disabled", true);
+      if (requestMarkAsSolvedButton) {
+        requestMarkAsSolvedButton.innerText = requestMarkAsSolvedButton.getAttribute('data-solve-translation');
+      }
+      requestCommentSubmitButton.disabled = true;
     }
   });
 
   // Disable submit button if textarea is empty
-  if ($requestCommentTextarea.val() === "") {
-    $requestCommentSubmitButton.prop("disabled", true);
+  if (requestCommentTextarea && requestCommentTextarea.value === '') {
+    requestCommentSubmitButton.disabled = true;
   }
 
   // Submit requests filter form in the request list page
-  $("#request-status-select, #request-organization-select")
-    .on("change", function() {
+  [].forEach.call(document.querySelectorAll('#request-status-select, #request-organization-select'), function(el) {
+    el.addEventListener('change', function() {
       search();
     });
+  });
 
   // Submit requests filter form in the request list page
-  $("#quick-search").on("keypress", function(e) {
-    if (e.which === 13) {
+  const quickSearch = document.querySelector('#quick-search')
+  quickSearch && quickSearch.addEventListener('keypress', function(e) {
+    if (e.which === 13) { // Enter key
       search();
     }
   });
 
   function search() {
+    const quickSearch = document.querySelector('#quick-search');
+    const requestStatusSelect = document.querySelector('#request-status-select');
+    const requestOrganizationSelect = document.querySelector('#request-organization-select');
+
     window.location.search = $.param({
-      query: $("#quick-search").val(),
-      status: $("#request-status-select").val(),
-      organization_id: $("#request-organization-select").val()
+      query: quickSearch && quickSearch.value,
+      status: requestStatusSelect && requestStatusSelect.value,
+      organization_id: requestOrganizationSelect && requestOrganizationSelect.value
     });
   }
 
   function toggleNavigation(toggleElement) {
-    var menu = document.getElementById("user-nav");
-    var isExpanded = menu.getAttribute("aria-expanded") === "true";
-    menu.setAttribute("aria-expanded", !isExpanded);
-    toggleElement.setAttribute("aria-expanded", !isExpanded);
+    var menu = document.getElementById('user-nav');
+    var isExpanded = menu.getAttribute('aria-expanded') === 'true';
+    menu.setAttribute('aria-expanded', !isExpanded);
+    toggleElement.setAttribute('aria-expanded', !isExpanded);
   }
 
-  $(".header .icon-menu").on("click", function(e) {
+  const burgerMenu = document.querySelector('.header .icon-menu');
+  const userMenu = document.querySelector('#user-nav');
+
+  burgerMenu.addEventListener('click', function(e) {
     e.stopPropagation();
     toggleNavigation(this);
   });
 
-  $(".header .icon-menu").on("keyup", function(e) {
+  burgerMenu.addEventListener('keyup', function(e) {
     if (e.keyCode === 13) { // Enter key
       e.stopPropagation();
       toggleNavigation(this);
     }
   });
 
-  $("#user-nav").on("keyup", function(e) {
+  userMenu.addEventListener('keyup', function(e) {
     if (e.keyCode === 27) { // Escape key
       e.stopPropagation();
-      this.setAttribute("aria-expanded", false);
-      $(".header .icon-menu").attr("aria-expanded", false);
+      this.setAttribute('aria-expanded', false);
+      burgerMenu.setAttribute('aria-expanded', false);
     }
   });
 
-  if ($("#user-nav").children().length === 0) {
-    $(".header .icon-menu").hide();
+  if (userMenu.children.length === 0) {
+    burgerMenu.style.display = 'none';
   }
 
   // Submit organization form in the request page
-  $("#request-organization select").on("change", function() {
-    this.form.submit();
-  });
+  const requestOrganisationSelect = document.querySelector('#request-organization select');
+
+  if (requestOrganisationSelect) {
+    requestOrganisationSelect.addEventListener('change', function() {
+      closest(this, 'form').submit();
+    });
+  }
 
   // Toggles expanded aria to collapsible elements
-  $(".collapsible-nav, .collapsible-sidebar").on("click", function(e) {
-    e.stopPropagation();
-    var isExpanded = this.getAttribute("aria-expanded") === "true";
-    this.setAttribute("aria-expanded", !isExpanded);
+  [].forEach.call(document.querySelectorAll('.collapsible-nav, .collapsible-sidebar'), function(el) {
+    el.addEventListener('click', function(e) {
+      e.stopPropagation();
+      var isExpanded = this.getAttribute('aria-expanded') === 'true';
+      this.setAttribute('aria-expanded', !isExpanded);
+    });
   });
 });
