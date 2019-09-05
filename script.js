c5f5c6ee312b0b83ec7af44d1ab54b0d0e50f0a5
Jiyu Zhang <jiyzhang@zendesk.com>
2019-09-05 08:50:03 +0200

    Move parentheses inside result count and fix the layout in rtl

--- script.js
+++ script.js
@@ -204,7 +204,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // If multibrand search has more than 5 help centers or categories collapse the list
   const multibrandFilterLists = document.querySelectorAll(".multibrand-filter-list");
   Array.prototype.forEach.call(multibrandFilterLists, function(filter) {
-    if (filter.children.length > 5) {
+    if (filter.children.length > 6) {
       // Display the show more button
       var trigger = filter.querySelector(".see-all-filters");
       trigger.setAttribute("aria-hidden", false);
