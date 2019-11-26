f635f3e32955ddcb0953124921c5d2a7824ffebd
Angela Cesere <angela.cesere@ithaka.org>
2019-11-26 12:42:10 -0500

    added a program to automatically update product branch with correct files

---
+++ update-for-product.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+declare -r PRODUCT=$1
+
+[[ "$PRODUCT" ]] || { echo "Please specify a product like 'jstor'"; exit 1; }
+
+[[ -f manifest.json ]] || { echo "Please run this command from the root dir"; exit 1; }
+
+cp manifest_${PRODUCT}.json manifest.json
+cp styles/_variables_${PRODUCT}.scss styles/_variables.scss
+cp styles/index_${PRODUCT}.scss styles/index.scss
+cp assets/favicon_${PRODUCT}.ico settings/favicon.ico
+cp assets/logo_${PRODUCT}.png settings/logo.png
