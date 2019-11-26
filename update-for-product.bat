63038f1a3c47bde712efe867e85dd42e4bde7a2a
Angela Cesere <angela.cesere@ithaka.org>
2019-11-26 13:49:36 -0500

    adding the program that replaces files by product in windows

---
+++ update-for-product.bat
@@ -0,0 +1,12 @@
+IF [%1]==[] (
+	ECHO Input a product
+	EXIT 1
+)
+
+SET PRODUCT=%1
+
+copy manifest_%PRODUCT%.json manifest.json
+copy styles\_variables_%PRODUCT%.scss styles\_variables.scss
+copy styles\index_%PRODUCT%.scss styles\index.scss
+copy assets\favicon_%PRODUCT%.ico settings\favicon.ico
+copy assets\logo_%PRODUCT%.png settings\logo.png
