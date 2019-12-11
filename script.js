2cd06d89f2a883241225a2316860e12aed587d21
Marc Høegh <mhoegh90@gmail.com>
2019-12-11 15:54:46 +0100

    script.js: Auto focus fields that have error notifications under them

--- script.js
+++ script.js
@@ -211,4 +211,14 @@ document.addEventListener('DOMContentLoaded', function() {
       })
     }
   });
+
+  // If there are any error notifications below an input field, focus that field
+  const notificationElm = document.querySelector(".notification-error");
+  if (
+    notificationElm &&
+    notificationElm.previousElementSibling &&
+    typeof notificationElm.previousElementSibling.focus === "function"
+  ) {
+    notificationElm.previousElementSibling.focus();
+  }
 });
