3cbf94319548eceaa6817c6d9330ab8620b448e7
zarma <zarif.madraimov@confiugra.com>
2019-12-30 16:52:59 +0800

    [web-4903] Notification Banner

--- script.js
+++ script.js
@@ -431,3 +431,20 @@ function handleRebootOrRefresh(){
 /**** END CUSTOM CHAT ****/
 
 
+/**** NOTIFICATION BANNER ****/
+
+$.get( "/api/v2/help_center/"+$('html').attr('lang').toLowerCase()+"/articles.json?label_names=alert" ).done(function( data ) {
+     
+  $.each(data.articles, function(index,item) {
+    
+    var style1 = '<div class="ns-box ns-bar ns-effect-slidetop ns-type-notice ns-show"><div class="ns-box-inner"><span class="megaphone"></span></i><p><a href="'+ item.html_url + '">' + item.title + '</a>' + item.body + '</p></div><span class="ns-close"></span></div>'
+          
+    $('.alertbox').append(style1);
+  });
+  $('.ns-close').on('click',function(){
+   $(".alertbox").remove();
+ });
+   
+ });
+
+/**** END NOTIFICATION BANNER ****/
