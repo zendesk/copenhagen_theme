91dd908510f7964d2f1adf3d0c2cb52ac99b8ad9
André Alves <andrenpalves@gmail.com>
2020-03-03 17:53:40 +0100

    Update script

--- bin/validate-version.sh
+++ bin/validate-version.sh
@@ -1,17 +1,22 @@
+#!/bin/sh
+
+# EXIT ON ERROR
+set -e
+
 # GET BRANCH MANIFEST VERSION
 BRANCH_VERSION=$(jq -r  '.version' manifest.json)
 
 # GET MASTER MANIFEST VERSION
 MASTER_VERSION=$(git show origin/master:manifest.json | jq -r  '.version')
 
-echo "$MASTER_VERSION -> $BRANCH_VERSION"
-
 # COMPARE VERSIONS
 LOWER_VERSION=$(printf '%s\n' "$BRANCH_VERSION" "$MASTER_VERSION" | sort -V | head -n1)
 
- if [ "$BRANCH_VERSION" = "$LOWER_VERSION" ]; then 
-        echo "Please bump your manifest version."
-        exit 1;
- else
-        echo "Manifest version was bumped."
- fi
+echo "$MASTER_VERSION -> $BRANCH_VERSION"
+
+if [ "$BRANCH_VERSION" = "$LOWER_VERSION" ]; then
+    echo "Please bump your manifest version."
+    exit 1;
+else
+    echo "Manifest version was bumped."
+fi
