2a2c08cac75fc8c3e8e7bc83d491212f2ae96f3f
hidha <hidemi.hamanaka@configura.com>
2020-06-09 19:10:31 +0800

    added table of contents on articles

--- sidebar/js/index.tsx
+++ sidebar/js/index.tsx
@@ -3,14 +3,14 @@ import * as ReactDOM from "react-dom";
 import Sidebar from "./components/Sidebar";
 
 function domReady(callback: () => void) {
-  document.readyState === "interactive" || document.readyState === "complete"
-    ? callback()
-    : document.addEventListener("DOMContentLoaded", callback);
+	document.readyState === "interactive" || document.readyState === "complete"
+		? callback()
+		: document.addEventListener("DOMContentLoaded", callback);
 }
 
 domReady(() => {
-  ReactDOM.render(
-    <Sidebar />,
-    document.body.appendChild(document.createElement("DIV"))
-  );
+	ReactDOM.render(
+		<Sidebar />,
+		document.body.appendChild(document.createElement("DIV"))
+	);
 });
