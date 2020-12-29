cef7993a38e26903eb030513c7c5454cbad913e2
Matt Hall <matt@zapnito.com>
2020-12-29 09:23:53 +0000

    Install PostCSS

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('postcss-nested'),
+    require('autoprefixer'),
+    require('cssnano')
+  ]
+}
