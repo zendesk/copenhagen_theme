7f95d4fa985ce0b44b62f34116f4f9de3270cb8c
Jen Gorfine <jen.gorfine@zendesk.com>
2022-02-24 17:01:02 -0500

    feat: removed tabindex from button + tidied inner page styles

--- script.js
+++ script.js
@@ -40,7 +40,6 @@ function buildClearSearchButton() {
   const button = document.createElement("button");
   button.setAttribute("type", "button");
   button.setAttribute("aria-controls", searchInput.id);
-  button.setAttribute("tabindex", "-1");
   button.classList.add("clear-button");
   const icon = `<svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' focusable='false' role='img' viewBox='0 0 12 12' aria-label='${window.searchClearButtonLabel}'><path stroke='currentColor' stroke-linecap='round' stroke-width='2' d='M3 9l6-6m0 6L3 3'/></svg>`;
   button.innerHTML = icon;
