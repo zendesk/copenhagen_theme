bc95c9ea117c7acbbee7cc7952c05141cc384b88
Julio Lozovei <jlozovei@gmail.com>
2022-11-03 15:20:27 -0300

    feat: highlight current section on category page

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,13 @@
 import { menu } from './modules/menu';
 import { collapse } from './modules/collapse';
+import { subjectsList } from './modules/subjectsList';
 
 (() => {
   const app = {
     init() {
       menu.init();
       collapse.init();
+      subjectsList.init();
     }
   };
 
