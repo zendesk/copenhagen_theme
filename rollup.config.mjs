89a2c368b9ea6b7b12c7321c1cd2c25202211b64
Gianluca Frediani <gianluca.frediani@zendesk.com>
2023-07-04 16:35:33 +0200

    chore: added node_modules and react support to rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,8 @@
 import zass from "./zass.mjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
+import replace from "@rollup/plugin-replace";
 
 export default {
   input: "src/index.js",
@@ -6,7 +10,22 @@ export default {
     file: "script.js",
     format: "iife",
   },
-  plugins: [zass()],
+  plugins: [
+    zass(),
+    nodeResolve({
+      extensions: [".js", "jsx"],
+    }),
+    babel({
+      babelHelpers: "bundled",
+      presets: ["@babel/preset-react"],
+      extensions: [".js"],
+    }),
+    commonjs(),
+    replace({
+      preventAssignment: true,
+      "process.env.NODE_ENV": '"production"',
+    }),
+  ],
   watch: {
     clearScreen: false,
   },
