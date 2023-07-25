32df3eeaed4e0bbd020d2091a81e2444e90ea8a7
Gianluca Frediani <gianluca.frediani@zendesk.com>
2023-07-25 12:51:49 +0200

    feat: added Garden subject field in new request form

---
+++ src/modules/new-request-form/fields/TextInput.tsx
@@ -0,0 +1,29 @@
+import {
+  Field as GardenField,
+  Hint,
+  Input,
+  Label,
+  Message,
+} from "@zendeskgarden/react-forms";
+import type { Field } from "../data-types";
+
+interface TextInputProps {
+  field: Field;
+}
+
+export function TextInput({ field }: TextInputProps): JSX.Element {
+  const { label, error, value, name, required, description } = field;
+  return (
+    <GardenField>
+      <Label>{label}</Label>
+      {description && <Hint>{description}</Hint>}
+      <Input
+        name={name}
+        value={value}
+        validation={error ? "error" : undefined}
+        required={required}
+      />
+      {error && <Message validation="error">{error}</Message>}
+    </GardenField>
+  );
+}
