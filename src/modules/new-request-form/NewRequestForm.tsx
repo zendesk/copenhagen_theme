88d82aa8d22bddc16cd84bddcd2785acabbab82a
Luis Almeida <lmg.almeida@gmail.com>
2023-07-27 15:53:55 +0200

    feat: handling priority field

--- src/modules/new-request-form/NewRequestForm.tsx
+++ src/modules/new-request-form/NewRequestForm.tsx
@@ -1,5 +1,6 @@
 import type { Field, TicketForm } from "./data-types";
 import { TextInput } from "./fields/TextInput";
+import { DropDown } from "./fields/DropDown";
 import { TicketFormField } from "./ticket-form-field/TicketFormField";
 
 export interface NewRequestFormProps {
@@ -15,6 +16,8 @@ export function NewRequestForm({ ticketForms, fields }: NewRequestFormProps) {
         switch (field.type) {
           case "subject":
             return <TextInput field={field} />;
+          case "priority":
+            return <DropDown field={field} />;
           default:
             return <></>;
         }
