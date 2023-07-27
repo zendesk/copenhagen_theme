de9228f2d121ea996bdd09e56a09f3972a10f7d4
Luis Almeida <lmg.almeida@gmail.com>
2023-07-27 16:07:31 +0200

    chore: built assets

---
+++ assets/shared.js
@@ -0,0 +1,11 @@
+import { D as DEFAULT_THEME, j as jsxRuntimeExports, e as ThemeProvider } from 'vendor';
+
+let theme = DEFAULT_THEME;
+function setupGardenTheme(callback) {
+    theme = callback(DEFAULT_THEME);
+}
+function ComponentProviders({ children }) {
+    return jsxRuntimeExports.jsx(ThemeProvider, { theme: theme, children: children });
+}
+
+export { ComponentProviders, setupGardenTheme };
