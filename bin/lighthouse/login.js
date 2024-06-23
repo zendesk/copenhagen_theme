ccbc5d1fcbd821ce188ec6db390d135416fd1f5d
raycasimiro <raycasimiro@gmail.com>
2024-06-23 15:06:27 +0800

    style: update manifest, homepage, and footer

--- bin/lighthouse/login.js
+++ bin/lighthouse/login.js
@@ -1,8 +1,13 @@
 async function login(browser, account) {
   const { email, password, loginUrl } = account;
-  
+
+  const ua =
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36";
+
   const page = await browser.newPage();
+  await page.setUserAgent(ua);
   await page.goto(loginUrl);
+  console.log("Login page loaded, attempting to log in...");
   await page.waitForSelector("input#user_email", { visible: true });
   await page.type("input#user_email", email);
   await page.type("input#user_password", password);
