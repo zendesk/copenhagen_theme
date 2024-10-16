59b6d587c55a0fc276517f39d68dbbe034950880
Małgorzata Bień <malgorzata.bien@zendesk.com>
2024-10-16 13:10:36 +0200

    feat: add service catalog item page

--- assets/service-catalog-bundle.js
+++ assets/service-catalog-bundle.js
@@ -1,4 +1,4 @@
-import { s as styled, J as getColorV8, j as jsxRuntimeExports, aa as SvgShapesFill, ab as Col, ac as Grid, r as reactExports, a as useTranslation, ad as Row, ae as CursorPagination, a6 as reactDomExports, a7 as ThemeProviders, a8 as createTheme } from 'shared';
+import { s as styled, J as getColorV8, j as jsxRuntimeExports, aa as SvgShapesFill, ab as Col, ac as Grid, r as reactExports, a as useTranslation, ad as Row, ae as CursorPagination, a6 as reactDomExports, a7 as ThemeProviders, a8 as createTheme, af as XXXL, _ as Button, ag as SvgChevronUpFill, ah as SvgChevronDownFill } from 'shared';
 
 const StyledItem = styled.a `
   display: flex;
@@ -16,7 +16,7 @@ const StyledItem = styled.a `
     border-color: ${(props) => getColorV8("blue", 600, props.theme)};
   }
 `;
-const ItemTitle = styled.div `
+const ItemTitle$1 = styled.div `
   font-size: ${(props) => props.theme.fontSizes.md};
   font-weight: ${(props) => props.theme.fontWeights.semibold};
   display: -webkit-box;
@@ -50,7 +50,7 @@ const IconContainer = styled.div `
   align-content: center;
 `;
 const ServiceCatalogListItem = ({ serviceItem, }) => {
-    return (jsxRuntimeExports.jsxs(StyledItem, { href: "#", children: [jsxRuntimeExports.jsx(IconContainer, { children: jsxRuntimeExports.jsx(SvgShapesFill, {}) }), jsxRuntimeExports.jsxs(TextContainer, { children: [jsxRuntimeExports.jsx(ItemTitle, { children: serviceItem.name }), jsxRuntimeExports.jsx(ItemDescription, { children: serviceItem.description })] })] }));
+    return (jsxRuntimeExports.jsxs(StyledItem, { href: "#", children: [jsxRuntimeExports.jsx(IconContainer, { children: jsxRuntimeExports.jsx(SvgShapesFill, {}) }), jsxRuntimeExports.jsxs(TextContainer, { children: [jsxRuntimeExports.jsx(ItemTitle$1, { children: serviceItem.name }), jsxRuntimeExports.jsx(ItemDescription, { children: serviceItem.description })] })] }));
 };
 
 const StyledCol = styled(Col) `
@@ -106,4 +106,62 @@ async function renderServiceCatalogList(container, settings) {
     reactDomExports.render(jsxRuntimeExports.jsx(ThemeProviders, { theme: createTheme(settings), children: jsxRuntimeExports.jsx(ServiceCatalogList, {}) }), container);
 }
 
-export { renderServiceCatalogList };
+const ItemTitle = styled(XXXL) `
+  font-weight: ${(props) => props.theme.fontWeights.semibold};
+`;
+const CollapsibleDescription = styled.div `
+  font-size: ${(props) => props.theme.fontSizes.md};
+  text-align: left;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: ${(props) => (props.expanded ? "none" : 3)};
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 100%;
+  margin-top: ${(props) => props.theme.space.md};
+`;
+const Container = styled.div `
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 120px;
+`;
+const LeftColumn = styled.div `
+  flex: 2;
+  border-bottom: ${(props) => props.theme.borders.sm}
+    ${(props) => getColorV8("grey", 300, props.theme)};
+  padding-bottom: ${(props) => props.theme.space.lg};
+`;
+const RightColumn = styled.div `
+  flex: 1;
+`;
+const ToggleButton = styled(Button) `
+  margin-top: ${(props) => props.theme.space.sm};
+  color: ${(props) => getColorV8("blue", 600, props.theme)};
+  font-size: ${(props) => props.theme.fontSizes.md};
+  &:hover {
+    text-decoration: none;
+    color: ${(props) => getColorV8("blue", 600, props.theme)};
+  }
+`;
+function ServiceCatalogItemPage({ serviceCatalogItem, }) {
+    const [isExpanded, setIsExpanded] = reactExports.useState(false);
+    const { t } = useTranslation();
+    const toogleDescription = () => {
+        setIsExpanded(!isExpanded);
+    };
+    return (jsxRuntimeExports.jsxs(Container, { children: [jsxRuntimeExports.jsxs(LeftColumn, { children: [jsxRuntimeExports.jsx(ItemTitle, { children: serviceCatalogItem.name }), jsxRuntimeExports.jsx(CollapsibleDescription, { expanded: isExpanded, children: serviceCatalogItem.description }), jsxRuntimeExports.jsxs(ToggleButton, { isLink: true, onClick: toogleDescription, children: [isExpanded
+                                ? t("service-catalog.item.read-less", "Read less")
+                                : t("service-catalog.item.read-more", "Read more"), jsxRuntimeExports.jsx(Button.EndIcon, { children: isExpanded ? jsxRuntimeExports.jsx(SvgChevronUpFill, {}) : jsxRuntimeExports.jsx(SvgChevronDownFill, {}) })] })] }), jsxRuntimeExports.jsx(RightColumn, {})] }));
+}
+
+const serviceCatalogItem = {
+    id: 1,
+    name: "Apple MacBook Pro",
+    description: "Request for a new Apple MacBook Pro. The MacBook Pro is equipped with Apple's powerful M3 Pro chip, featuring a 12-core CPU, 18-core GPU, and a 16-core Neural Engine, making it ideal for high-performance tasks. It comes with a 140W USB-C power adapter, three Thunderbolt 4 ports, HDMI, SDXC card slot, headphone jack, and MagSafe 3 port. The backlit Magic Keyboard with Touch ID enhances security and usability. Exclusively for Engineering, Design, and Marketing departments, the 16-inch model includes up to 36+ GB of memory and 1+ TB of storage, while other departments can request machines with up to 36 GB of memory and 512 GB of storage.",
+};
+async function renderServiceCatalogItem(container, settings) {
+    reactDomExports.render(jsxRuntimeExports.jsx(ThemeProviders, { theme: createTheme(settings), children: jsxRuntimeExports.jsx(ServiceCatalogItemPage, { serviceCatalogItem: serviceCatalogItem }) }), container);
+}
+
+export { renderServiceCatalogItem, renderServiceCatalogList };
---
+++ src/modules/service-catalog/ServiceCatalogItemPage.tsx
@@ -0,0 +1,86 @@
+import { XXXL } from "@zendeskgarden/react-typography";
+import type { ServiceCatalogItem } from "./data-types/ServiceCatalogItem";
+import styled from "styled-components";
+import { Button } from "@zendeskgarden/react-buttons";
+import { useState } from "react";
+import ChevronUp from "@zendeskgarden/svg-icons/src/16/chevron-up-fill.svg";
+import ChevronDown from "@zendeskgarden/svg-icons/src/16/chevron-down-fill.svg";
+import { getColorV8 } from "@zendeskgarden/react-theming";
+import { useTranslation } from "react-i18next";
+
+const ItemTitle = styled(XXXL)`
+  font-weight: ${(props) => props.theme.fontWeights.semibold};
+`;
+
+const CollapsibleDescription = styled.div<{ expanded: boolean }>`
+  font-size: ${(props) => props.theme.fontSizes.md};
+  text-align: left;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: ${(props) => (props.expanded ? "none" : 3)};
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 100%;
+  margin-top: ${(props) => props.theme.space.md};
+`;
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 120px;
+`;
+
+const LeftColumn = styled.div`
+  flex: 2;
+  border-bottom: ${(props) => props.theme.borders.sm}
+    ${(props) => getColorV8("grey", 300, props.theme)};
+  padding-bottom: ${(props) => props.theme.space.lg};
+`;
+
+const RightColumn = styled.div`
+  flex: 1;
+`;
+
+const ToggleButton = styled(Button)`
+  margin-top: ${(props) => props.theme.space.sm};
+  color: ${(props) => getColorV8("blue", 600, props.theme)};
+  font-size: ${(props) => props.theme.fontSizes.md};
+  &:hover {
+    text-decoration: none;
+    color: ${(props) => getColorV8("blue", 600, props.theme)};
+  }
+`;
+interface ServiceCatalogItemPageProps {
+  serviceCatalogItem: ServiceCatalogItem;
+}
+
+export function ServiceCatalogItemPage({
+  serviceCatalogItem,
+}: ServiceCatalogItemPageProps) {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+  const { t } = useTranslation();
+
+  const toogleDescription = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <Container>
+      <LeftColumn>
+        <ItemTitle>{serviceCatalogItem.name}</ItemTitle>
+        <CollapsibleDescription expanded={isExpanded}>
+          {serviceCatalogItem.description}
+        </CollapsibleDescription>
+        <ToggleButton isLink onClick={toogleDescription}>
+          {isExpanded
+            ? t("service-catalog.item.read-less", "Read less")
+            : t("service-catalog.item.read-more", "Read more")}
+          <Button.EndIcon>
+            {isExpanded ? <ChevronUp /> : <ChevronDown />}
+          </Button.EndIcon>
+        </ToggleButton>
+      </LeftColumn>
+      <RightColumn></RightColumn>
+    </Container>
+  );
+}
