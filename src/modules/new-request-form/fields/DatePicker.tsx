34ce8e9c3d4591a574d1ab2d0a4413c7be73cba6
Gianluca Frediani <gianluca.frediani@zendesk.com>
2024-10-29 15:35:41 +0100

    refactor: added useCallback for datepicker callbacks

--- src/modules/new-request-form/fields/DatePicker.tsx
+++ src/modules/new-request-form/fields/DatePicker.tsx
@@ -9,7 +9,7 @@ import {
 import { Span } from "@zendeskgarden/react-typography";
 import type { Field } from "../data-types";
 import type { ChangeEventHandler } from "react";
-import { useMemo, useState } from "react";
+import { useCallback, useState } from "react";
 
 interface DatePickerProps {
   field: Field;
@@ -29,44 +29,48 @@ export function DatePicker({
     value ? new Date(value as string) : undefined
   );
 
-  const dateTimeFormat = useMemo(
-    () =>
-      new Intl.DateTimeFormat(locale, {
-        month: "long",
-        day: "numeric",
-        year: "numeric",
-        timeZone: "UTC",
-      }),
-    [locale]
-  );
-
   /* Formats the date using the UTC time zone to prevent timezone-related issues.
    * By creating a new Date object with only the date, the time defaults to 00:00:00 UTC.
    * This avoids date shifts that can occur if formatted with the local time zone, as Garden does by default.
    */
-  const formatDateInput = (date: Date) => {
-    return dateTimeFormat.format(date);
-  };
+  const formatDateInput = useCallback(
+    (date: Date) => {
+      const dateTimeFormat = new Intl.DateTimeFormat(locale, {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+        timeZone: "UTC",
+      });
+      return dateTimeFormat.format(date);
+    },
+    [locale]
+  );
 
-  const formatDateValue = (value: Date | undefined) => {
-    if (value === undefined) {
-      return "";
-    }
-    const isoString = value.toISOString();
-    return valueFormat === "dateTime" ? isoString : isoString.split("T")[0];
-  };
+  const formatDateValue = useCallback(
+    (value: Date | undefined) => {
+      if (value === undefined) {
+        return "";
+      }
+      const isoString = value.toISOString();
+      return valueFormat === "dateTime" ? isoString : isoString.split("T")[0];
+    },
+    [valueFormat]
+  );
 
-  const handleChange = (date: Date) => {
-    // Set the time to 12:00:00 as this is also the expected behavior across Support and the API
-    const newDate = new Date(
-      Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 12, 0, 0)
-    ) as Date;
-    setDate(newDate);
-    const dateString = formatDateValue(newDate);
-    if (dateString !== undefined) {
-      onChange(dateString);
-    }
-  };
+  const handleChange = useCallback(
+    (date: Date) => {
+      // Set the time to 12:00:00 as this is also the expected behavior across Support and the API
+      const newDate = new Date(
+        Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 12, 0, 0)
+      ) as Date;
+      setDate(newDate);
+      const dateString = formatDateValue(newDate);
+      if (dateString !== undefined) {
+        onChange(dateString);
+      }
+    },
+    [onChange, formatDateValue]
+  );
 
   const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     // Allow field to be cleared
