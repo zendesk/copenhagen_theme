c2c1f67ff800c2e16fdd6fdabe9da85cf7e1b22f
BrunoBFerreira <bruno.ferreira@zendesk.com>
2025-02-28 13:51:12 +0000

    feat: add search to Services List

---
+++ src/modules/service-catalog/components/service-catalog-list-item/Search.tsx
@@ -0,0 +1,48 @@
+import { Field, Label, MediaInput } from "@zendeskgarden/react-forms";
+import { Dots } from "@zendeskgarden/react-loaders";
+import SearchIcon from "@zendeskgarden/svg-icons/src/16/search-stroke.svg";
+import { useTranslation } from "react-i18next";
+import styled from "styled-components";
+
+const StyledField = styled(Field)`
+  align-items: center;
+  width: 320px;
+  @media (max-width: 768px) {
+    width: 100%;
+    display: flex;
+  }
+`;
+
+interface SearchProps {
+  searchInputValue: string;
+  isLoading: boolean;
+  onChange: (value: string) => void;
+}
+
+export const Search = ({
+  searchInputValue,
+  isLoading,
+  onChange,
+}: SearchProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <StyledField>
+      <Label hidden>
+        {t("service-catalog.search-services", "Search for services")}
+      </Label>
+      <MediaInput
+        start={<SearchIcon />}
+        type="search"
+        autoComplete="off"
+        end={isLoading && searchInputValue ? <Dots /> : undefined}
+        value={searchInputValue}
+        placeholder={t(
+          "service-catalog.search-services",
+          "Search for services"
+        )}
+        onChange={(event) => onChange(event.target.value)}
+      />
+    </StyledField>
+  );
+};
