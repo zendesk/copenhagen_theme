7cd98932e51dccd3d413279424ac7ed41b3e84ee
katiesfz <56288271+katiesfz@users.noreply.github.com>
2025-05-20 16:05:04 +0800

    1.0.69

---
+++ assets/table-of-contents.js
@@ -0,0 +1,14 @@
+/* TABLE OF CONTENTS */
+
+const headings = Array.from(document.getElementById("main-content").getElementsByTagName('h2', 'h3'));
+const tocContainer = document.querySelector(".table-of-contents");
+const ul = document.createElement("ul");
+
+tocContainer.appendChild(ul);
+headings.map((heading) => {
+    const id = heading.innerText.toLowerCase().replaceAll(" ", "_");
+    heading.setAttribute("id", id);
+    const anchorElement = `<a href="#${id}">${heading.textContent}</a>`;
+    const keyPointer = `<li>${anchorElement}</li>`;
+    ul.insertAdjacentHTML("beforeend", keyPointer);
+});
