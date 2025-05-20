a3fc07755ed9008a821e075a2c81766438598fe6
katiesfz <56288271+katiesfz@users.noreply.github.com>
2025-05-20 16:08:55 +0800

    1.0.70

--- assets/table-of-contents.js
+++ assets/table-of-contents.js
@@ -1,14 +1,18 @@
 /* TABLE OF CONTENTS */
+    
+document.addEventListener("DOMContentLoaded", function(event) {
+    
+    const headings = Array.from(document.getElementById("main-content").getElementsByTagName('h2', 'h3'));
+    const tocContainer = document.querySelector(".table-of-contents");
+    const ul = document.createElement("ul");
 
-const headings = Array.from(document.getElementById("main-content").getElementsByTagName('h2', 'h3'));
-const tocContainer = document.querySelector(".table-of-contents");
-const ul = document.createElement("ul");
+    tocContainer.appendChild(ul);
+    headings.map((heading) => {
+        const id = heading.innerText.toLowerCase().replaceAll(" ", "_");
+        heading.setAttribute("id", id);
+        const anchorElement = `<a href="#${id}">${heading.textContent}</a>`;
+        const keyPointer = `<li>${anchorElement}</li>`;
+        ul.insertAdjacentHTML("beforeend", keyPointer);
+    });
 
-tocContainer.appendChild(ul);
-headings.map((heading) => {
-    const id = heading.innerText.toLowerCase().replaceAll(" ", "_");
-    heading.setAttribute("id", id);
-    const anchorElement = `<a href="#${id}">${heading.textContent}</a>`;
-    const keyPointer = `<li>${anchorElement}</li>`;
-    ul.insertAdjacentHTML("beforeend", keyPointer);
 });
