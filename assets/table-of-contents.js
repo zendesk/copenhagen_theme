d308dd33bdd52401a88dd9e8aae91e118c22f7bb
katiesfz <56288271+katiesfz@users.noreply.github.com>
2025-05-21 14:51:25 +0800

    1.0.77

--- assets/table-of-contents.js
+++ assets/table-of-contents.js
@@ -5,13 +5,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
     const headings = Array.from(document.getElementById("main-content").getElementsByTagName('h2', 'h3'));
     const tocContainer = document.querySelector(".table-of-contents");
     const ul = document.createElement("ul");
+    const heading = document.createElement("h4");
 
+    heading.innerText = "Table of contents";
+
+    tocContainer.appendChild(heading);
     tocContainer.appendChild(ul);
     headings.map((heading) => {
         const id = heading.innerText.toLowerCase().replaceAll(" ", "_");
         heading.setAttribute("id", id);
         const anchorElement = `<a href="#${id}">${heading.textContent}</a>`;
-        const keyPointer = `<li>${anchorElement}</li>`;
+        if (index === 0) {
+            const keyPointer = `<li class="active">${anchorElement}</li>`;
+        } else {
+            const keyPointer = `<li>${anchorElement}</li>`;
+        }
         ul.insertAdjacentHTML("beforeend", keyPointer);
     });
 
@@ -21,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     
     const headings = Array.from(document.getElementById("main-content").getElementsByTagName('h2', 'h3'));
     const tocContainer = document.querySelector(".table-of-contents");
-    const tocAnchors = tocContainer.querySelectorAll("a");
+    const tocAnchors = tocContainer.querySelectorAll("li");
     const obFunc = (entries) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
