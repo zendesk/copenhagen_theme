06091703020f56c67cd47b6ca6116421718fe3eb
Kate Gasiorek <kate.gasiorek@zendesk.com>
2025-08-07 15:10:23 +0200

    feat: add link to useNotify

--- src/modules/shared/notifications/useNotify.tsx
+++ src/modules/shared/notifications/useNotify.tsx
@@ -7,17 +7,30 @@ import {
 import type { ToastNotification } from "./ToastNotification";
 import { useCallback } from "react";
 import { useTranslation } from "react-i18next";
+import { Anchor } from "@zendeskgarden/react-buttons";
+import styled from "styled-components";
+
+const StyledLink = styled(Anchor)`
+  text-decoration: underline;
+  display: block;
+  margin-top: ${props => props.theme.space.xxs}px;
+`;
 
 export const useNotify = () => {
   const { addToast } = useToast();
   const { t } = useTranslation();
 
   const notify = useCallback(
-    ({ title, message, type }: ToastNotification) => {
+    ({ title, message, type, link }: ToastNotification) => {
       addToast(({ close }) => (
         <Notification type={type}>
           {title && <Title>{title}</Title>}
-          {message}
+            {message}
+            {link && (
+              <StyledLink href={link.href}>
+                {link.text}
+              </StyledLink>
+            )}
           <Close
             aria-label={t("cph-theme-shared.close-label", "Close")}
             onClick={close}
