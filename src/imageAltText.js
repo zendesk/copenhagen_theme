74bd2e454b67312a4b78dd6868d27efe5bb2b95e
Małgorzata Duda <malgorzata.duda@zendesk.com>
2025-09-15 15:46:23 +0200

    style: add alt text for images in posts/articles

---
+++ src/imageAltText.js
@@ -0,0 +1,38 @@
+/**
+ * Adds default alt text to images that don't have one
+ */
+function addAltTextToImages() {
+  const articleBody = document.querySelector(".article-body");
+  const postBody = document.querySelectorAll(".post-body");
+
+  if (articleBody) {
+    const allImages = articleBody.querySelectorAll("img");
+    const imagesWithoutAlt = articleBody.querySelectorAll(
+      "img:not([alt]), img[alt='']"
+    );
+
+    imagesWithoutAlt.forEach((img) => {
+      const position = Array.from(allImages).indexOf(img) + 1;
+      const altText = "Image " + position + " in article";
+      img.setAttribute("alt", altText);
+    });
+  }
+
+  const post = postBody ? postBody[0] : null;
+  if (post) {
+    const allImages = post.querySelectorAll("img");
+    const imagesWithoutAlt = post.querySelectorAll(
+      "img:not([alt]), img[alt='']"
+    );
+
+    imagesWithoutAlt.forEach((img) => {
+      const position = Array.from(allImages).indexOf(img) + 1;
+      const altText = "Image " + position + " in post";
+      img.setAttribute("alt", altText);
+    });
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  addAltTextToImages();
+});
