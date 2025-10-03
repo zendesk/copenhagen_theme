b6799128a75a6bb550f07f17288806a71327c11c
Gianluca Frediani <gianluca.frediani@zendesk.com>
2025-10-03 17:40:47 +0200

    chore: fix theme upload script for lighthouse

--- bin/theme-upload.js
+++ bin/theme-upload.js
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-env node */
 const brandId = process.env.BRAND_ID;
 const { execSync } = require("child_process");
 
 function zcli(command) {
-  const data = execSync(`yarn --silent zcli ${command} --json`);
+  const data = execSync(`yarn zcli ${command} --json`);
   return JSON.parse(data.toString());
 }
 
