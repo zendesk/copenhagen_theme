4f9d8cdb04914a13133280853489ba9927577848
Małgorzata Bień <malgorzata.bien@zendesk.com>
2025-11-20 15:25:21 +0100

    chore: fix tests warnings and update needed packages

--- jest.config.mjs
+++ jest.config.mjs
@@ -5,6 +5,7 @@ const config = {
   transform: {
     "^.+.tsx?$": ["ts-jest", {}],
   },
+  setupFiles: ["<rootDir>/jest.i18n.setup.ts"],
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
 };
 
---
+++ jest.i18n.setup.ts
@@ -0,0 +1,2 @@
+import { initI18next } from "./src/modules/shared/i18n/initI18next";
+initI18next("en");
