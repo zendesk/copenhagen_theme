845543430752987c96f12b4b5bb47a575cb1b0c2
João Vieira <joao.vieira@zendesk.com>
2025-12-11 15:20:15 +0000

    fix: improve answerBotGenerativeExperience null check

--- src/modules/new-request-form/NewRequestForm.tsx
+++ src/modules/new-request-form/NewRequestForm.tsx
@@ -159,7 +159,8 @@ export function NewRequestForm({
     !!answerBot?.request_id;
 
   const answerBotGenerativeModalEnabled =
-    !!answerBotGenerativeExperience?.request_id;
+    answerBotGenerativeExperience &&
+    Boolean(answerBotGenerativeExperience.request_id);
 
   return (
     <>
