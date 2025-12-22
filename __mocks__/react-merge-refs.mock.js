6d597daf15346003871b3cd85608bf5c821689d7
Melissa Real <melissa.real@hotmail.com>
2025-12-22 15:10:05 +0000

    chore: add mocks folder for svgs to pass tests

---
+++ __mocks__/react-merge-refs.mock.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mergeRefs: (refs) => (value) => {
+    refs.forEach((ref) => {
+      if (typeof ref === "function") {
+        ref(value);
+      } else if (ref != null) {
+        ref.current = value;
+      }
+    });
+  },
+};
--- jest.config.mjs
+++ jest.config.mjs
@@ -7,6 +7,9 @@ const config = {
   },
   transformIgnorePatterns: ["node_modules/(?!(react-merge-refs)/)"],
   setupFilesAfterEnv: ["@testing-library/jest-dom", "<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "\\.svg$": "<rootDir>/__mocks__/svg.mock.js",
+  },
 };
 
 export default config;
