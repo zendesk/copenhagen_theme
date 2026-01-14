44ceb702973f0911f3f5cc0040fd8add4182d091
Melissa Real <melissa.real@hotmail.com>
2026-01-14 16:05:25 +0000

    chore: remove shadowRoot rendering

--- src/modules/shared/garden-shadow/src/index.ts
+++
@@ -1,4 +0,0 @@
-import { ShadowRootContainer, useShadowRoot } from "./shadow-root-container";
-import { useDownshiftEnvironment } from "./useDownshiftEnvironment";
-
-export { ShadowRootContainer, useShadowRoot, useDownshiftEnvironment };
