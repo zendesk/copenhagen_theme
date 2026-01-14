ef5785c7b76c0e362fa0d7a3c123eb03136a63ce
Melissa Real <melissa.real@hotmail.com>
2026-01-14 16:05:25 +0000

    chore: remove shadowRoot rendering

--- src/modules/request-list/renderRequestList.tsx
+++ src/modules/request-list/renderRequestList.tsx
@@ -1,8 +1,7 @@
 import type { HCData } from "./data-types/hc/HCData";
-import { ShadowRootContainer } from "../shared/garden-shadow/src/shadow-root-container";
 import { createTheme } from "../shared/garden-theme/createTheme";
 import { ThemeProviders } from "../shared/garden-theme/ThemeProviders";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import { RequestsList } from "./components/requests-list/RequestsList";
 import { PushNotificationContext } from "./contexts/PushNotification";
 import { initI18next, loadTranslations } from "../shared/i18n";
@@ -30,22 +29,20 @@ export async function renderRequestList(
 
   const helpCenterPath = `/hc/${locale}`;
 
-  ReactDOM.render(
-    <ShadowRootContainer
-      mode={inCypress ? "open" : "closed"}
+  render(
+    <ThemeProviders
+      theme={createTheme(settings)}
       data-test-id={inCypress ? "guide-requests-app" : undefined}
     >
-      <ThemeProviders theme={createTheme(settings)}>
-        <ErrorBoundary helpCenterPath={helpCenterPath}>
-          <PushNotificationContext.Provider value={pushNotification}>
-            <RequestsList
-              locale={locale}
-              customStatusesEnabled={hasCustomStatusesEnabled}
-            />
-          </PushNotificationContext.Provider>
-        </ErrorBoundary>
-      </ThemeProviders>
-    </ShadowRootContainer>,
+      <ErrorBoundary helpCenterPath={helpCenterPath}>
+        <PushNotificationContext.Provider value={pushNotification}>
+          <RequestsList
+            locale={locale}
+            customStatusesEnabled={hasCustomStatusesEnabled}
+          />
+        </PushNotificationContext.Provider>
+      </ErrorBoundary>
+    </ThemeProviders>,
     container
   );
 }
